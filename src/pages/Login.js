5cbb42e9c2199c1fda9ac61bfffcebe5a78f7027
Bino <binob831@gmail.com>
2024-06-19 22:42:28 +0530

    update login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,6 +1,6 @@
 // src/pages/Login.js
 import React, { useState } from 'react';
-import api from '../api/axios';
+import api from '../api/api';
 
 const Login = () => {
   const [email, setEmail] = useState('');
@@ -12,6 +12,7 @@ const Login = () => {
       const res = await api.post('/api/user/login', { email, password });
       localStorage.setItem('authToken', res.data.token);
       // Redirect to dashboard or another page
+      window.location.href = '/dashboard';
     } catch (err) {
       console.error('Login failed', err);
     }
